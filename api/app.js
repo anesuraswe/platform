58ad474708da5152673a13590c30370eaf25d8e7
Anesu Nyamajiwa <147626052+anesuraswe@users.noreply.github.com>
2025-04-30 18:24:34 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,28 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const seatRoutes = require('./routes/seatRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
-const ticketRoutes = require('./routes/ticketRoutes');
-const errorHandler = require('./utils/errorHandler');
+import express from "express";
+import eventRoute from "./routes/events.route.js";
 
 const app = express();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
+app.use("/api/events", eventRoute);
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/seats', seatRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/payments', paymentRoutes);
-app.use('/api/tickets', ticketRoutes);
-
-// Error handling middleware
-app.use(errorHandler);
-
-module.exports = app;
+app.listen(8800, () => {
+  console.log("Server raita baba!!");
+});
